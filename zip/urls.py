1fde19ad40ddb0cfe4d513198446718fb45d888c
chettykulkarni <chettykulkarni@gmail.com>
2020-04-02 15:15:37 -0700

    adding new forms and improving systems

--- zip/urls.py
+++ zip/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
+    path('cars/', views.car_view),
+    path('home', views.home_view, name='home'),
+    path('create_car', views.car_create_view),
 ]
--- zip/views.py
+++ zip/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render
 from .models import Car,User
+from django.http import HttpResponse
+
+from .forms import CarForm
 
 # Create your views here.
 
-def post_list(request):
+def car_create_view(request):
+    form = CarForm(request.POST or None)
+    if form.is_valid():
+        form.save
+    
+    context={
+        'form' : form
+    }
+    return render(request,'zip/car_create.html', context)
+
+def home_view(*args,**kwargs):
+    return HttpResponse("<h1> Hello World!1 </h1>")
+
+def car_view(request):
+    
+    # context={}
     cars=Car.objects.all()
-    users=User.objects.all()
-    return render(request, 'zip/post_list.html', {'cars': cars,'users':users})
+    return render(request, 'zip/car_detail.html', {'cars': cars})
