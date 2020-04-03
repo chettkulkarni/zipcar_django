d8c27294c908ca1d0afa409cb00218410e748899
chettykulkarni <chettykulkarni@gmail.com>
2020-04-02 18:18:36 -0700

    heruk deployment

--- zipcar/settings.py
+++ zipcar/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'yiml(g$gz#)@9p=bcfwyw*=jhusxqjx4&isas-9iivjbn85qaz'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.herukoapp.com','*']
 
 
 # Application definition
